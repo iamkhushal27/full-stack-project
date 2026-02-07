571de450be1c9c42a8dc7c886d80a912ad5b22ca
iamkhushal27 <157139749+iamkhushal27@users.noreply.github.com>
2026-02-07 20:11:48 +0500

    database connection is made

---
+++ backend/src/models/index.js
@@ -0,0 +1,65 @@
+const { Sequelize } = require("sequelize");
+const dbConfig = require("../configs").database;
+const fs = require("fs");
+const path = require("path");
+
+const basename = path.basename(__filename);
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    dialect: dbConfig.dialect,
+    logging: dbConfig.logging,
+    define: {
+      timestamps: true,
+    },
+    dialectOptions: {
+      ...dbConfig.dialectOptions,
+      statement_timeout: 60000,
+      connectionTimeoutMillis: 60000,
+    },
+    pool: dbConfig.pool,
+  }
+);
+
+const db = {};
+
+fs.readdirSync(__dirname)
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.error(
+      "Warning: Could not establish database connection during initialization:",
+      err.message
+    );
+  });
+
+module.exports = db;

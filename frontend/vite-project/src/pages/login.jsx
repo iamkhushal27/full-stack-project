e3710b798261f3a7c219bf641494ccfbae3e4a73
iamkhushal27 <157139749+iamkhushal27@users.noreply.github.com>
2026-02-09 18:38:29 +0500

    frontend layout of login is made

--- frontend/vite-project/src/pages/login.jsx
+++ frontend/vite-project/src/pages/login.jsx
@@ -11,8 +11,8 @@ import {
   Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-
-
+import img from "../assets/R 2.png";
+import background from "../assets/background.png";
 
 function Login() {
   const form = useForm({
@@ -30,45 +30,88 @@ function Login() {
   });
 
   return (
-    <Box h="100vh" w="100wh" bg="#6741d9">
-      
-      <Flex justify="center" h="100%" align="center" direction="column" gap="xl"> 
-      <Title order={1} c="white">Welcome to the website</Title>
-        <Paper p="xl">
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
-          <TextInput
-            withAsterisk
-            label="Name"
-            placeholder="Enter your name"
-            key={form.key("name")}
-            {...form.getInputProps("name")}
-          />
-          <TextInput
-            withAsterisk
-            label="Email"
-            placeholder="your@email.com"
-            key={form.key("email")}
-            {...form.getInputProps("email")}
-          />
-          <PasswordInput
-            withAsterisk
-            label="Passwrod"
-            placeholder="Password"
-            key={form.key("password")}
-            {...form.getInputProps("passsword")}
-          />
+    <Box
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#FF6767",
+      }}
+    >
+      <Flex align="center" h="100vh" justify="center">
+        <Paper h="80vh" w="80vw">
+          <Flex justify="space-between">
+            <Box
+              h="80vh"
+              w="30vw"
+              style={{
+                display: "flex",
+                justifyContent: "center", // horizontal alignment (optional)
+                alignItems: "flex-end", // vertical alignment → pushes image to the bottom
+              }}
+            >
+              <img
+                style={{
+                  width: "100%",
+                  height: "85%",
+                  objectFit: "fill", // stretches image to fully fill the container
+                }}
+                src={img}
+              ></img>
+            </Box>
 
-          <Checkbox
-            mt="md"
-            label="I agree to sell my privacy"
-            key={form.key("termsOfService")}
-            {...form.getInputProps("termsOfService", { type: "checkbox" })}
-          />
+            <Flex
+              justify="center"
+              h="100%"
+              align="center"
+              direction="column"
+              gap="xl"
+            >
+              <Title order={1} c="white">
+                Welcome to the website
+              </Title>
+              <Paper p="xl">
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                  <TextInput
+                    withAsterisk
+                    label="Name"
+                    placeholder="Enter your name"
+                    key={form.key("name")}
+                    {...form.getInputProps("name")}
+                  />
+                  <TextInput
+                    withAsterisk
+                    label="Email"
+                    placeholder="your@email.com"
+                    key={form.key("email")}
+                    {...form.getInputProps("email")}
+                  />
+                  <PasswordInput
+                    withAsterisk
+                    label="Passwrod"
+                    placeholder="Password"
+                    key={form.key("password")}
+                    {...form.getInputProps("passsword")}
+                  />
 
-          <Group justify="flex-end" mt="md">
-            <Button type="submit">Submit</Button>
-          </Group>
-        </form>
+                  <Checkbox
+                    mt="md"
+                    label="I agree to sell my privacy"
+                    key={form.key("termsOfService")}
+                    {...form.getInputProps("termsOfService", {
+                      type: "checkbox",
+                    })}
+                  />
+
+                  <Group justify="flex-end" mt="md">
+                    <Button type="submit">Submit</Button>
+                  </Group>
+                </form>
+              </Paper>
+            </Flex>
+          </Flex>
         </Paper>
       </Flex>
     </Box>

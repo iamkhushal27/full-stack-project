2ba4cfec9293427d6f910e7e24521ec43820e4c2
iamkhushal27 <157139749+iamkhushal27@users.noreply.github.com>
2026-02-10 16:57:06 +0500

    layout is made

--- frontend/vite-project/src/pages/registration.jsx
+++ frontend/vite-project/src/pages/registration.jsx
@@ -1,83 +1,143 @@
+import {
+  Button,
+  Checkbox,
+  Group,
+  TextInput,
+  Box,
+  Flex,
+  PasswordInput,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import img from "../assets/R 2.png";
+import background from "../assets/background.png";
 
+function Registration() {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
 
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      confirmPassword: (value, values) =>
+        value !== values.password ? "Passwords do not match" : null,
+    },
+  });
 
-  import {
-    Button,
-    Checkbox,
-    Group,
-    TextInput,
-    Box,
-    Flex,
-    PasswordInput,
-    Paper,
-    Text,
-    Title,
-  } from "@mantine/core";
-  import { useForm } from "@mantine/form";
-  
-  
-  
-  function Registration() {
-    const form = useForm({
-      mode: "uncontrolled",
-      initialValues: {
-        Name: "",
-        email: "",
-        password: "",
-        termsOfService: false,
-      },
-  
-      validate: {
-        email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      },
-    });
-  
-    return (
-      <Box h="100vh" w="100wh" bg="#6741d9">
-        
-        <Flex justify="center" h="100%" align="center" direction="column" gap="xl"> 
-        <Title order={1} c="white">Welcome to the registraion Page</Title>
-          <Paper p="xl">
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            <TextInput
-              withAsterisk
-              label="Name"
-              placeholder="Enter your name"
-              key={form.key("name")}
-              {...form.getInputProps("name")}
-            />
-            <TextInput
-              withAsterisk
-              label="Email"
-              placeholder="your@email.com"
-              key={form.key("email")}
-              {...form.getInputProps("email")}
-            />
-            <PasswordInput
-              withAsterisk
-              label="Passwrod"
-              placeholder="Password"
-              key={form.key("password")}
-              {...form.getInputProps("passsword")}
-            />
-  
-            <Checkbox
-              mt="md"
-              label="I agree to sell my privacy"
-              key={form.key("termsOfService")}
-              {...form.getInputProps("termsOfService", { type: "checkbox" })}
-            />
-  
-            <Group justify="flex-end" mt="md">
-              <Button type="submit">Submit</Button>
-            </Group>
-          </form>
-          </Paper>
-        </Flex>
-      </Box>
-    );
-  }
-  
-  
+  return (
+    <Box
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#FF6767",
+      }}
+    >
+      <Flex align="center" h="100vh" justify="center">
+        <Paper h="80vh" w="80vw">
+          <Flex justify="space-between">
+            <Box
+              h="80vh"
+              w="30vw"
+              style={{
+                display: "flex",
+                justifyContent: "center", // horizontal alignment (optional)
+                alignItems: "flex-end", // vertical alignment → pushes image to the bottom
+              }}
+            >
+              <img
+                style={{
+                  width: "100%",
+                  height: "85%",
+                  objectFit: "fill", // stretches image to fully fill the container
+                }}
+                src={img}
+              ></img>
+            </Box>
 
+            <Flex
+              justify="center"
+              h="80vh"
+              w="35vw"
+              align="center"
+              direction="column"
+              gap="xl"
+            >
+              <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                <Flex gap="lg" direction="column">
+                  <TextInput
+                    withAsterisk
+                    placeholder="Enter your name"
+                    key={form.key("name")}
+                    {...form.getInputProps("name")}
+                    w="30vw"
+                    styles={{
+                      input: {
+                        border: "1px solid #565454",
+                        padding: "20px",
+                      },
+                    }}
+                  />
+                  <TextInput
+                    withAsterisk
+                    placeholder="your@email.com"
+                    key={form.key("email")}
+                    {...form.getInputProps("email")}
+                    w="30vw"
+                    styles={{
+                      input: {
+                        border: "1px solid #565454",
+                        padding: "20px",
+                      },
+                    }}
+                  />
+                  <PasswordInput
+                    withAsterisk
+                    placeholder="Password"
+                    key={form.key("password")}
+                    {...form.getInputProps("password")}
+                    w="30vw"
+                    styles={{
+                      input: {
+                        border: "1px solid #565454",
+                        padding: "20px",
+                      },
+                    }}
+                  />
+                  <PasswordInput
+                    withAsterisk
+                    placeholder="Confirm Password"
+                    {...form.getInputProps("confirmPassword")}
+                    w="30vw"
+                    styles={{
+                      input: {
+                        border: "1px solid #565454",
+                        padding: "20px",
+                      },
+                    }}
+                  />
+                </Flex>
+
+                <Group mt="md">
+                  <Button type="submit" bg="#FF9090" radius="sm">
+                    Register
+                  </Button>
+                </Group>
+              </form>
+            </Flex>
+          </Flex>
+        </Paper>
+      </Flex>
+    </Box>
+  );
+}
 export default Registration;
